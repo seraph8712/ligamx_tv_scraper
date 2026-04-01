71af255f040d5eec742bbc449be38d6112936d1e
Sergio <sergioave.87@gmail.com>
2026-04-01 14:32:55 -0600

    Se corrige script run_scraper.sh, tenía una palabra incorrecta al final de la linea 6

--- scripts/run_scraper.sh
+++ scripts/run_scraper.sh
@@ -3,4 +3,4 @@
 #se mueve a la carpeta anterior donde esta localizado main.py
 cd "$(dirname "$0")/.." || exit 1
 
-.venv/bin/python main.py >> scraper.log 2>&1git 
+.venv/bin/python main.py >> scraper.log 2>&1
